58094c53f47245501ed468eb9eea1e4378f42169
Antti Kontoniemi <antti.kontoniemi@gmail.com>
2018-11-04 10:12:52 +0200

    Update .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  "extends": "airbnb",
-  "rules": {
-    "semi": [2, "never"],
-    "max-len": ['error', 120 ],
-    "react/jsx-filename-extension": ["off"],
-    "react/no-array-index-key": ["off"],
-    "import/no-unresolved": ["off"],
-    "import/extensions": ["off"]
-  }
+    "extends": "airbnb",
+    "rules": {
+        "react/destructuring-assignment": ["off", {"ignoreClassFields": true}],
+        "semi": [2, "never"],
+        "max-len": ['error', 120],
+        "react/jsx-filename-extension": ["off"],
+        "react/no-array-index-key": ["off"],
+        "import/no-unresolved": ["off"],
+        "import/extensions": ["off"]
+    }
 };
